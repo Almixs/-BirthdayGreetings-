37130262eee5fd17ceb592688fedc75b5e3f56ba
Danylo <Chornobaidan@gmail.com>
2023-08-20 17:11:55 +0300

    v1.2
    a system of transferring the birthday output from Saturday, Sunday -> Monday was made

--- main.py
+++ main.py
@@ -14,15 +14,20 @@ def get_birthdays_per_week(users):
         birthday_people = [user["name"] for user in users if user["birthday"].month == target_day.month and user["birthday"].day == target_day.day]
 
         if birthday_people:
-            print(f"{day_name}:", ", ".join(birthday_people))
+            if day_name == "Sunday":
+                print("Monday:", ", ".join(birthday_people))
+            elif day_name == "Saturday":
+                print("Monday:", ", ".join(birthday_people))
+            else:
+                print(f"{day_name}:", ", ".join(birthday_people))
 
 users = [
-    {"name": "people1", "birthday": datetime.datetime(2000, 8, 16)},
-    {"name": "people2", "birthday": datetime.datetime(1999, 8, 18)},
-    {"name": "people3", "birthday": datetime.datetime(1998, 8, 20)},
-    {"name": "people4", "birthday": datetime.datetime(1997, 8, 21)},
-    {"name": "people5", "birthday": datetime.datetime(1996, 8, 24)},
-    {"name": "people6", "birthday": datetime.datetime(2000, 8, 16)},
+    {"name": "people1", "birthday": datetime.datetime(2000, 8, 20)},
+    {"name": "people2", "birthday": datetime.datetime(1999, 8, 15)},
+    {"name": "people3", "birthday": datetime.datetime(1998, 8, 23)},
+    {"name": "people4", "birthday": datetime.datetime(1997, 8, 25)},
+    {"name": "people5", "birthday": datetime.datetime(1996, 8, 26)},
+    {"name": "people6", "birthday": datetime.datetime(2000, 8, 20)},
     {"name": "people7", "birthday": datetime.datetime(2001, 8, 22)},
 ]
 
